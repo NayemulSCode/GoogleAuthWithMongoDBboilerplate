e7cbd8271a51b8e515eb98eb764f3e09f14947ac
Nayemul <nayemulhasan2016@gmail.com>
2020-10-15 01:20:11 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const passport = require('passport')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
+
+//load config
+dotenv.config({ path: './config/config.env' })
+
+//passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express()
+
+
+//logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+//midlewair Handlebars
+app.engine('.hbs',exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+
+//sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
